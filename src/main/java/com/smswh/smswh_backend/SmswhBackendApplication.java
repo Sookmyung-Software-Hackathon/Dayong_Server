bb3ba13f2b9a5ae951a22f06675db1d480b8cb16
ChaeyeonHan <kate2236e@naver.com>
2022-08-24 17:34:32 +0900

    Initial commit

---
+++ src/main/java/com/smswh/smswh_backend/SmswhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smswh.smswh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmswhBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmswhBackendApplication.class, args);
+    }
+
+}
